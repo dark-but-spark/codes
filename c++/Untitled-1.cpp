066a5a287b8f680264d183e6e13e3d40e5e6fcd1
chen jiyan <3036789610@qq.com>
2024-12-06 17:55:30 +0800

    modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	modified:   c++/Untitled-1.cpp 	modified:   c++/Untitled-1.exe 	renamed:    java/test.java -> java/Matrixtest.java

--- c++/Untitled-1.cpp
+++ c++/Untitled-1.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main()
-{  printf("hello");
+{  
+    map<char,int> m;
+    m['s']=1;
+    printf("%d",m['s']);
+    
+    printf("hello");
     return 0;
 }
--- java/Matrixtest.java
+++ java/Matrixtest.java
@@ -58,9 +58,7 @@ class Matrix{
     public Matrix plus(Matrix that) throws Exception
     {
         if(this.row!=that.row||this.colunm!=that.colunm)
-        {
-            throw new Exception("fuck add");
-        }
+            throw new Exception("fuck plus or minus");
         else 
         {
             double result[][]=new double[this.row][this.colunm];
@@ -71,9 +69,45 @@ class Matrix{
             return new Matrix(this.row,this.colunm,result);
        }
     }
+    public Matrix multiply(double x) throws Exception //x*this
+    {
+        double result[][]= new double[this.row][this.colunm];
+        for(int i=0;i<this.row;i++) for(int j=0;j<this.colunm;j++)
+        {
+            result[i][j]=x*this.a[i][j];
+        }
+        return new Matrix(this.row,this.colunm,result);
+    }
+    public Matrix minus(Matrix that) // this - that
+    {
+        try
+        {
+            Matrix res=this.plus(that.multiply(-1));
+            return res;
+        }
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+            return  new Matrix();
+        }
 
+    }
+    public Matrix multiply(Matrix that) throws Exception // this * that 
+    {
+        if(this.colunm!=that.row) 
+            throw new Exception("fuck multiply");
+        else
+        {
+            double res[][]= new double[this.row][that.colunm]
+            for(int i=0;i<this.row;i++) for(int j=0;j<that.colunm;j++) for(int k=0;k<that.row;k++)
+            {
+                res[i][j]+=this.a[i][k]*this.a[k][j];
+            }
+            return new Matrix(this.row,that.colunm,res);
+        }
+    }
 }
-class text{
+class Matrixtest{
     public static void main(String[] args) {
         double a[][] = {{1, 2, 1}, {2, 2, 3}, {2, 2, 0}};
         double b[][] = {{2}, {0}, {0}};
@@ -86,12 +120,21 @@ class text{
         Matrix D = new Matrix(d.length, d[0].length, d);
         Matrix E = new Matrix("I",2);
        
-            System.out.println("A:" + A.print());
-            System.out.println("B:" + B.print());
-            System.out.println("C:" + C.print());
-            System.out.println("D:" + D.print());
-            System.out.println("A+C:"+A.plus(C).print());
-            System.out.println("B+D:"+B.plus(D).print());
+            System.out.println("A=" + A.print());
+            System.out.println("B=" + B.print());
+            System.out.println("C=" + C.print());
+            System.out.println("D=" + D.print());
+            System.out.println("E=" + E.print());
+            System.out.println("A+C="+A.plus(C).print());
+            // System.out.println("A+E="+A.plus(E).print());
+            System.out.println("B+D="+B.plus(D).print());
+            System.out.println("3*A="+A.multiply(3).print());
+            System.out.println("A-C="+A.minus(C).print());
+            System.out.println("B-D="+B.minus(D).print());
+            System.out.println("A-D="+A.minus(D).print());
+            System.out.println("A*C="+A.multiply(C).print());
+            System.out.println("A*B="+A.multiply(B).print());
+            System.out.println("B*A="+B.multiply(A).print());
         }
         catch (Exception e) 
         {
