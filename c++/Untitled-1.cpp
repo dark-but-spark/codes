e1be10421c3454804c94715a4bf99ff7f41c7e6a
chen jiyan <3036789610@qq.com>
2024-12-07 18:55:42 +0800

    modified:   c++/Untitled-1.cpp 	modified:   exe/Untitled-1.exe

--- c++/Untitled-1.cpp
+++ c++/Untitled-1.cpp
@@ -5,7 +5,13 @@ int main()
     map<char,int> m;
     m['s']=1;
     printf("%d",m['s']);
-    
+    char* c="aef";
+    if(c=="a") puts("YES");
+    else  puts("NO");
+    if(c+1 =="e")
+        printf("lzy");
+    if(c+2 =="f")
+        puts("nb !!!!");
     printf("hello");
     return 0;
 }
