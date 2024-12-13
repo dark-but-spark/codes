ac291b8d8774ed08c0e75df3ed6e1ac69f78066f
chen jiyan <3036789610@qq.com>
2024-12-13 11:02:54 +0800

    new file:   c++/calculate.cpp 	new file:   exe/calculate.exe

---
+++ c++/calculate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+double e=1e-9,dx=1e-8;
+double a=1,b=2;
+
+
+inline double f(double x)
+{
+    return x*x;
+}
+
+int main()
+{
+    double x=a,sum=0;
+    while(x<b+e)
+    {
+        sum+=dx*.5*(f(x+dx)+f(x));
+        x=x+dx;
+    }
+    printf("%lf",sum-7.0/3);
+        
+}
