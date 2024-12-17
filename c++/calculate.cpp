7b2fa5bb4746b721716a1cf8627f52c9f6d9e8a5
chen jiyan <3036789610@qq.com>
2024-12-17 19:06:52 +0800

    modified:   c++/calculate.cpp     modified:   exe/calculate.exe

--- c++/calculate.cpp
+++ c++/calculate.cpp
@@ -12,18 +12,29 @@ inline double f(double x)
 int main()
 {
     double x=a,sum=0;
-    while(x+dx<=b+e)
+    // while(x+dx<=b+e)
+    // {
+    //     sum+=dx*.5*(f(x+dx)+f(x));
+    //     x=x+dx;
+    // }
+    // printf("%lf\n",sum);
+    // x=a,sum=0;
+    // while(x+2*dx<=b+e)
+    // {
+    //     sum+=(dx/3)*(f(x)+4*f(x+dx)+f(x+2*dx));
+    //     x+=2*dx;
+    // }
+    // printf("%lf",sum);
+    sum=0;
+    int n=2;
+    while(1)
     {
-        sum+=dx*.5*(f(x+dx)+f(x));
-        x=x+dx;
+        sum=0;
+        n*=2;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=(i*sin(1+i/n)*sin(1+i/n))/(n*n);
+        }
+        printf("%d %lf\n",n,sum);
     }
-    printf("%lf\n",sum);
-    x=a,sum=0;
-    while(x+2*dx<=b+e)
-    {
-        sum+=(dx/3)*(f(x)+4*f(x+dx)+f(x+2*dx));
-        x+=2*dx;
-    }
-    printf("%lf",sum);
-        
 }
