a58176575a2205c331a3bab4a80465ae76b1c67c
chen jiyan <3036789610@qq.com>
2024-12-17 09:31:46 +0800

    modified:   c++/calculate.cpp 	modified:   exe/calculate.exe

--- c++/calculate.cpp
+++ c++/calculate.cpp
@@ -1,21 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-double e=1e-9,dx=1e-8;
-double a=1,b=2;
+double e=1e-9,dx=.5;
+double a=2,b=4;
 
 
 inline double f(double x)
 {
-    return x*x;
+    return 1.0/((x-1)*(x-1));
 }
 
 int main()
 {
     double x=a,sum=0;
-    while(x<b+e)
+    while(x+dx<=b+e)
     {
         sum+=dx*.5*(f(x+dx)+f(x));
         x=x+dx;
+    }
+    printf("%lf\n",sum);
+    x=a,sum=0;
+    while(x+2*dx<=b+e)
+    {
+        sum+=(dx/3)*(f(x)+4*f(x+dx)+f(x+2*dx));
+        x+=2*dx;
     }
     printf("%lf",sum);
         
