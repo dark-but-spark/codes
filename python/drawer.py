502986458d63d52b1a650c974297281669ae0fab
chen jiyan <3036789610@qq.com>
2025-02-21 11:37:59 +0800

    modified:   java/Main.java 	modified:   python/drawer.py

--- python/drawer.py
+++ python/drawer.py
@@ -3,9 +3,18 @@ import math
 # a=int(input())
 # b=int(input())
 # print(a+b)
-x=5
+x=1
+n=int(1e5)
 # print(math.tan(1)) 
 while(1):
+    sum=0
+    # for i in range(1,n+1):
+        # sum+=i/(n*n+n+i)
+    print((n+(n+2*n**0.5)**0.5)**0.5-n**0.5)
+    print((n+n**0.5+1)**0.5
+          -n**0.5)
+    n*=2
     input()
-    x=math.log(math.exp(x)/x-1.0/x)
-    print(x)
+
+    # x=x-(math.tan(x)-1)*(math.cos(x)*math.cos(x))
+    # print(x/math.pi)
